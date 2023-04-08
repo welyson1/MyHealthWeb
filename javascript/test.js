2eaed749ad04115013eda472de81447e5fec6cdb
Welyson <53984581+welyson1@users.noreply.github.com>
2023-04-08 16:23:48 -0300

    Inicial

---
+++ javascript/test.js
@@ -0,0 +1,11 @@
+let lista = []
+
+console.log(lista.length)
+
+for (let index = 0; index < 5; index++) {
+    let element = prompt("Digita:");  
+
+    lista.push(element);
+}
+
+console.log(lista);
