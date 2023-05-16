1b0fedaf7070eee20f54c29a8c581f178b526fad
welysondev <53984581+welyson1@users.noreply.github.com>
2023-05-16 18:15:01 -0300

    aaa

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -1,6 +1,3 @@
-import { auth } from "../firebase/config.js";
-import { createUser } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
-
 window.onload = () => {
     const senhaInput = document.getElementById("pwCriarSenha");
     const confirmarSenhaInput = document.getElementById("pwRepetirCriarSenha");
